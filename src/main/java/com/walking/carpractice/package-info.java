f314a06caf6d69966356270a6086815d85e35583
ааа <@d.d>
2024-09-02 22:01:01 +0400

    Technical task

--- src/main/java/com/walking/carpractice/package-info.java
+++ src/main/java/com/walking/carpractice/package-info.java
@@ -1,1 +1,11 @@
+/**
+ * Доработайте задачу из
+ * <a href="https://github.com/KFalcon2022/lessons/blob/master/lessons/web-and-java-ee/147/Servlet%20API.%20ServletConfig.%20ServletContext.%20Listeners.md">предыдущей статьи</a>,
+ * реализовав сериализацию для тел ответов в фильтрах.
+ * <p>
+ * Опциональное усложнение: обеспечьте в фильтре и десериализацию для тел запросов в возможном формате - в `Map` или
+ * целевые объекты. Последнее, в зависимости от имплементации, может потребовать большего погружения в Jackson, нежели
+ * в посвященной ему
+ * <a href="https://github.com/KFalcon2022/lessons/blob/master/lessons/libraries-and-build-systems/145/FasterXML%20Jackson.md">статье</a>.
+ */
 package com.walking.carpractice;
